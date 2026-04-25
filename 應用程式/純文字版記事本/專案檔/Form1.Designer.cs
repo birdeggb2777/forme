e49cab1cb3531fe1a7df324a03b7cbe24420d08c
MSI\birdeggb2777 <birdeggb2777@gmail.com>
2026-04-26 00:21:34 +0800

    添加純文字版記事本專案檔

---
+++ 應用程式/純文字版記事本/專案檔/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace onlytext
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟舊檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.儲存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Window;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 44);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案ToolStripMenuItem
+            // 
+            this.檔案ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開啟舊檔ToolStripMenuItem,
+            this.儲存ToolStripMenuItem});
+            this.檔案ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.檔案ToolStripMenuItem.Name = "檔案ToolStripMenuItem";
+            this.檔案ToolStripMenuItem.Size = new System.Drawing.Size(89, 44);
+            this.檔案ToolStripMenuItem.Text = "檔案";
+            // 
+            // 開啟舊檔ToolStripMenuItem
+            // 
+            this.開啟舊檔ToolStripMenuItem.Name = "開啟舊檔ToolStripMenuItem";
+            this.開啟舊檔ToolStripMenuItem.Size = new System.Drawing.Size(315, 40);
+            this.開啟舊檔ToolStripMenuItem.Text = "開啟舊檔";
+            this.開啟舊檔ToolStripMenuItem.Click += new System.EventHandler(this.開啟舊檔ToolStripMenuItem_Click);
+            // 
+            // 儲存ToolStripMenuItem
+            // 
+            this.儲存ToolStripMenuItem.Name = "儲存ToolStripMenuItem";
+            this.儲存ToolStripMenuItem.Size = new System.Drawing.Size(315, 50);
+            this.儲存ToolStripMenuItem.Text = "儲存";
+            this.儲存ToolStripMenuItem.Click += new System.EventHandler(this.儲存ToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.textBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(16, 0, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(800, 406);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Font = new System.Drawing.Font("標楷體", 14.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox.Location = new System.Drawing.Point(16, 0);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(784, 406);
+            this.textBox.TabIndex = 2;
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "純文字版記事本";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開啟舊檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 儲存ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ 應用程式/純文字版記事本/專案檔/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace onlytext
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "";
+        public Form1(){ InitializeComponent(); }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(dragEnter);
+            this.DragDrop += new DragEventHandler(dragDrop);
+
+            this.WindowState = FormWindowState.Maximized;
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length == 2)
+            {
+                try
+                {
+                    string fileContent = System.IO.File.ReadAllText(args[1], System.Text.Encoding.UTF8);
+                    textBox.Text = fileContent;
+                    this.Text = filePath = args[1];
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("檔案讀取失敗，原因窩也不知道(╥﹏╥)");
+                }
+            }
+        }
+
+        private void 開啟舊檔ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "選擇文字檔案";
+            openFileDialog.Filter = "文字檔 (*.txt)|*.txt|文字檔 (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileContent = System.IO.File.ReadAllText(openFileDialog.FileName, System.Text.Encoding.UTF8);
+                    textBox.Text = fileContent;
+                    this.Text = filePath = openFileDialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("檔案讀取失敗，原因窩也不知道(╥﹏╥)");
+                }
+            }
+            else MessageBox.Show("檔案讀取失敗，原因窩也不知道(╥﹏╥)");
+        }
+
+        private void 儲存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = "文字檔 (*.txt)|*.txt|文字檔 (*.*)|*.*";
+            saveFileDialog.Title = "儲存檔案";
+            saveFileDialog.DefaultExt = "txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, "" + textBox.Text);
+                    filePath = saveFileDialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("檔案讀取失敗，原因窩也不知道(╥﹏╥)");
+                }
+            }
+        }
+        private async void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (filePath != "" && e.Control && e.KeyCode == Keys.S)
+            {
+                File.WriteAllText(filePath, "" + textBox.Text);
+                this.Text = "儲存完畢";
+                await Task.Delay(1000);
+                this.Text = filePath;
+            }
+        }
+        private void dragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+            else e.Effect = DragDropEffects.None;
+        }
+        private void dragDrop(object sender, DragEventArgs e)
+        {
+            if (filePath != "") return;
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length == 1)
+            {
+                string fileContent = System.IO.File.ReadAllText(files[0], System.Text.Encoding.UTF8);
+                textBox.Text = fileContent;
+                this.Text = filePath = files[0];
+            }
+        }
+    }
+}
